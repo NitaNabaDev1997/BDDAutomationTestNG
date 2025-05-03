e537e413b9995e25da900d7fcf3325900aa8cf0d
NitaNabaDev1997 <nitanabadev@gmail.com>
2025-05-03 13:28:54 +0530

    updated

--- src/test/java/ParallelTestNGRunner.java
+++ src/test/java/ParallelTestNGRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(features = "src/test/resources/features/ContactUs.feature",
         glue = "StepDefinitions",
         //tags = "@chapter1Validation",
-        plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber-json"
+        plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber.json"
                 ,"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
                 ,"timeline:test-output-thread/"
                 ,"rerun:target/failedrerun.txt"},
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/resources/features/ContactUs.feature",
         glue = "StepDefinitions",
         //tags = "@chapter1Validation",
-        plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber-json"
+        plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber.json"
         ,"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true,publish = true)
 public class TestRunner {
